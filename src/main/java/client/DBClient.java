7bfcf3600d4733678f3d46b58a505a7ac52e9721
Shripad <shripada@thoughtworks.com>
2016-05-24 08:15:12 +0530

    Shirpad #Added completion stages to run things in parallel.

--- src/main/java/client/DBClient.java
+++ src/main/java/client/DBClient.java
@@ -7,6 +7,7 @@ import factory.ApplicationFactory;
 import rpc.RPCClient;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
 public class DBClient {
 
@@ -18,8 +19,8 @@ public class DBClient {
         this.client = client;
     }
 
-    public void put(String key, String value) throws IOException, ClassNotFoundException, InterruptedException {
-        client.doNetworkRpc(new DBEntry(key, value));
+    public DBOperationResponse put(String key, String value) throws IOException, ClassNotFoundException, InterruptedException {
+        return client.doNetworkRpc(new DBEntry(key, value));
     }
 
   public String get(String key) throws IOException, ClassNotFoundException, InterruptedException {
@@ -28,10 +29,29 @@ public class DBClient {
 
       return  dbOperationResponse.getResponse();
     }
+    private static void doSome(){
+        CompletableFuture<String> future = new CompletableFuture<>();
+        CompletableFuture<String> stringFuture = CompletableFuture.supplyAsync( () -> {
+            System.out.println("Blah"); return "sdsd";});
+        CompletableFuture<String> intFuture = CompletableFuture.supplyAsync( () -> "10");
+        CompletableFuture<String> stringCompletableFuture = stringFuture.thenCombine(intFuture, (x, y) -> x + " " + y);
+//        Function<String, CompletableFuture<String>> o = new Function<String, CompletableFuture<String>>() {
+//
+//            @Override
+//            public CompletableFuture<String> apply(String s) {
+//                return intFuture;
+//            }
+//        };
+//        CompletableFuture<String> complete = stringFuture.thenCompose(o);
+//        System.out.println(complete.getNow("NADA"));
+//        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.allOf(intFuture, stringFuture).then;
+//        voidCompletableFuture.isDone();
+    }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         DBClient client = new DBClient(ApplicationFactory.getRPCClient());
-        client.put("nn","ddd");
+        DBOperationResponse put = client.put("nn", "ddd");
+        System.out.println(put.getResponse());
         client.put("nn2","ssss");
         client.put("nn3","C");
         client.put("qq","GGGGGGGG");
@@ -40,8 +60,10 @@ public class DBClient {
 
         System.out.println("#################################");
 
-        System.out.println(new DBClient(ApplicationFactory.simpleRPCClient("192.168.99.100",9999)).get("qq"));
-        System.out.println(new DBClient(ApplicationFactory.simpleRPCClient("192.168.99.100",9998)).get("qq"));
-        System.out.println(new DBClient(ApplicationFactory.simpleRPCClient("192.168.99.100",9997)).get("qq"));
+        System.out.println(new DBClient(ApplicationFactory.simpleRPCClient("localhost",9999)).get("qq"));
+        System.out.println(new DBClient(ApplicationFactory.simpleRPCClient("localhost",9998)).get("qq"));
+        System.out.println(new DBClient(ApplicationFactory.simpleRPCClient("localhost",9997)).get("qq"));
+//    doSome();
+
     }
 }
--- src/main/java/rpc/RPCClient.java
+++ src/main/java/rpc/RPCClient.java
@@ -13,10 +13,9 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.RecursiveTask;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.function.BiFunction;
 
 public class RPCClient {
 
@@ -29,35 +28,79 @@ public class RPCClient {
     public <T extends DBOperation> DBOperationResponse doNetworkRpc(T dbOperation) throws IOException {
 
         Token token = consistentHash.get(dbOperation.getKey());
-        System.out.println("######### " + token.getPosition());
+//        System.out.println("######### " + token.getPosition());
 
-        List<ForkJoinTask<DBOperationResponse>> response = new ArrayList<ForkJoinTask<DBOperationResponse>>();
+        List<CompletableFuture<DBOperationResponse>> response = new ArrayList<CompletableFuture<DBOperationResponse>>();
 
         int replication = 0;
+
+        CompletableFuture<String> future = new CompletableFuture<>();
+        future.complete("Done");
+
+        BiFunction<String, DBOperationResponse,String> bi = (x, y) -> {
+            if("Added".equals(y.getResponse())) {
+                System.out.println("In for loop Bi function");
+                return "Added";
+            }
+            else return "False";
+        };
+
         for(RemoteDBNode dbNode:token.getNodes()){
-            if(replication == 2) {
+            if(replication == 1) {
                 break;
             }
-            RPCTask rpcTask = new RPCTask(dbNode, dbOperation);
-            ForkJoinTask<DBOperationResponse> fork = rpcTask.fork();
-            response.add(fork);
+//            RPCTask rpcTask = new RPCTask(dbNode, dbOperation);
+//            ForkJoinTask<DBOperationResponse> fork = rpcTask.fork();
+//            response.add(fork);
+            CompletableFuture<DBOperationResponse> dbResponseFuture = new RPCFuture().getDBResponseFuture(dbNode, dbOperation);
+            dbResponseFuture.exceptionally(ex -> {
+                System.out.println("We have problem: " + ex.getMessage());
+                return new DBOperationResponse("Exception");
+            });
+//            response.add(dbResponseFuture);
+
+
+            CompletableFuture<String> completableFuture = future.thenCombineAsync(dbResponseFuture, bi);
+            future = completableFuture;
+
             replication ++;
 
         }
-
-        Stream<DBOperationResponse> dbOperationResponseStream = response.stream().map(task -> {
-            try {
-                return task.join();
-            } catch (Exception e) {
-                return new DBOperationResponse("Something wrong");
+        String resp = "";
+//        future.complete("Done");
+        while (!future.isDone()) {
+//            System.out.println("In loop");
+            if (future.isDone()) {
+                resp = future.getNow("False");
+                System.out.println("Done with future");
+                break;
             }
-        });
+        }
 
-        List<DBOperationResponse> collect = dbOperationResponseStream.collect(Collectors.toList());
+//
+//        Stream<DBOperationResponse> dbOperationResponseStream = response.stream().map( future -> {
+//            try {
+//                return future(new DBOperationResponse("NA"));
+//            } catch (Exception e) {
+//                return new DBOperationResponse("Error");
+//            }
+//        });
+
+//        List<DBOperationResponse> collect = dbOperationResponseStream.collect(Collectors.toList());
 
 
+        System.out.println("Returning response:" + resp);
+        return new DBOperationResponse(resp);
+    }
 
-        return collect.get(0);
+    private class RPCFuture {
+        public CompletableFuture<DBOperationResponse> getDBResponseFuture(RemoteDBNode node, DBOperation operation){
+            return CompletableFuture.supplyAsync(() -> {
+                DBOperationResponse dbOperationResponse = getDbOperationResponse(node, operation);
+                System.out.println("In RPC Future");
+                return dbOperationResponse; }
+            );
+        }
     }
 
     private class RPCTask extends RecursiveTask<DBOperationResponse>{
@@ -70,29 +113,38 @@ public class RPCClient {
             this.operation = operation;
         }
 
+
+
         @Override
         protected DBOperationResponse compute() {
-            System.out.println("In thread:" + dbNode.getPort());
-            Kryo kryo;
-            Output output;
-            DBOperationResponse dbOperationResponse = null;
-            try (Socket socket = new Socket(dbNode.getHost(), dbNode.getPort())) {
-                kryo = new Kryo();
-                output = new Output(socket.getOutputStream());
-                kryo.writeClassAndObject(output, operation);
-                kryo.writeClassAndObject(output, "#");
-                output.flush();
-
-                Input input = new Input(socket.getInputStream());
-                dbOperationResponse = kryo.readObject(input, DBOperationResponse.class);
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-                dbOperationResponse = new DBOperationResponse("Error in Network");
-            }
-
-
-            return dbOperationResponse;
+            return getDbOperationResponse(dbNode,operation);
         }
+
+
     };
+
+    private DBOperationResponse getDbOperationResponse(RemoteDBNode dbNode, DBOperation operation) {
+        System.out.println("In thread:" + dbNode.getPort());
+        Kryo kryo;
+        Output output;
+        DBOperationResponse dbOperationResponse = null;
+        try (Socket socket = new Socket(dbNode.getHost(), dbNode.getPort())) {
+            kryo = new Kryo();
+            output = new Output(socket.getOutputStream());
+            kryo.writeClassAndObject(output, operation);
+            kryo.writeClassAndObject(output, "#");
+            output.flush();
+
+            Input input = new Input(socket.getInputStream());
+            dbOperationResponse = kryo.readObject(input, DBOperationResponse.class);
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            dbOperationResponse = new DBOperationResponse("Error in Network");
+        }
+
+        System.out.println("Operation REsponse:" + dbOperationResponse.getResponse());
+
+        return dbOperationResponse;
+    }
 }
